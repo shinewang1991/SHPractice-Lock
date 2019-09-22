a89c589bd316e2c9068d579d10b406f8df21f9fa
wangxiang <wangxiang@yixia.com>
2019-09-22 16:37:43 +0800

    Initial Commit

---
+++ SHPractice-Lock/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SHPractice-Lock
+//
+//  Created by Shine on 2019/9/22.
+//  Copyright © 2019 shine. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
